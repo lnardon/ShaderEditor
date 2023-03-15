83a78039e7def0b5deffd229e0f5712c4ff19b4e
Lucas Nardon <lucaslnardon@gmail.com>
2023-03-15 05:19:45 -0300

    ✨ Add uTime shader uniform

--- src/components/ShaderMesh/ShaderMesh.tsx
+++ src/components/ShaderMesh/ShaderMesh.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 const ShaderMesh: React.FC<{ vertex: string; fragment: string }> = ({
   vertex,
   fragment,
 }) => {
+  const ref = useRef<any>();
+  useFrame((state) => {
+    let time = state.clock.getElapsedTime();
+    ref.current.material.uniforms.uTime.value = time;
+  });
+
   return (
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
+    <mesh ref={ref}>
+      <planeGeometry args={[1, 1, 10, 10]} />
       <rawShaderMaterial
         vertexShader={vertex}
         fragmentShader={fragment}
         needsUpdate={true}
         wireframe
+        uniforms={{ uTime: { value: 0 } }}
       />
     </mesh>
   );
--- src/utils/shaders.ts
+++ src/utils/shaders.ts
@@ -2,19 +2,22 @@ export const shaders = {
   vertex: `uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
+uniform float uTime;
 
 attribute vec3 position;
 
 void main()
 {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 0.350);
 }
 `,
   fragment: `precision mediump float;
+uniform float uTime;
+
 
 void main()
 {
-    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+    gl_FragColor = vec4(sin(uTime)/1.30, cos(uTime)/1.20, tan(uTime)/1.20, 1.0);
 }
 `,
 };
